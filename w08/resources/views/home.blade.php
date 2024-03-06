deb13b88f917c5a4492d020f4be44997d52e889b
Alicepim <126488371+Alicepim@users.noreply.github.com>
2024-03-06 13:11:46 +0700

    Add files via upload

---
+++ w08/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+@section('title')
+    
+@endsection
+@section('content')
+<div class="container mx-auto mt-8">
+    <!-- Example Card -->
+    <div class="max-w-md mx-auto bg-white rounded-md overflow-hidden shadow-md mt-6">
+        <img src="https://cdn.pixabay.com/photo/2023/11/12/18/52/christmas-8383920_1280.jpg" alt="Placeholder Image" class="w-full h-64 object-cover">
+        <div class="p-4">
+            <h3 class="text-xl font-bold mb-2">W08</h3>
+            <p class="text-gray-700">Warathep Tanyaruk</p>
+            <p class="text-gray-700">6414421016</p>
+        </div>
+    </div>
+</div>
+@endsection
